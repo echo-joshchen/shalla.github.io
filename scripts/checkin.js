a577cae7905b3013f45076eedf43edfbc276f5db
Josh <echojoshchen@gmail.com>
2014-12-27 00:24:44 -0800

    added sort order to checkout and checkin

--- scripts/checkin.js
+++ scripts/checkin.js
@@ -43,69 +43,6 @@ function addNewAddress()
     writeTableRow(newAddrData, index);
 }
 
-// Load database from a user selected file
-function loadDatabase()
-{
-    var f =  $('#dbfile').prop('files')[0];
-    var r = new FileReader();
-    r.onload = function(){readDbFile(r)};
-    r.readAsArrayBuffer(f);
-    
-    $("#checkin").show();
-}
-
-// Read the database file
-function readDbFile(fileReader)
-{
-    var Uints = new Uint8Array(fileReader.result);
-    db = new SQL.Database(Uints);
-        
-    // Clear existing menu
-    document.getElementById("termenu").innerHTML = "";
-    
-    // Read out territory names
-    var res = db.exec("SELECT tername FROM territory");
-    res[0].values.forEach(function(rowData) {
-        fillDropDownMenu(rowData);
-    });
-    $(".chosen-select").chosen();
-    
-    $("#termenu").change(createTerritoryTable);
-}
-
-function fillDropDownMenu(rowData)
-{
-    var menu = document.getElementById("termenu");
-    var terOption = document.createElement('option');
-    terOption.value = rowData[0];
-    terOption.innerHTML = rowData[0];
-    menu.appendChild(terOption);
-}
-
-function createTerritoryTable()
-{
-    // Clear current table and map
-    document.getElementById("tertable").innerHTML = "";
-    coords = [];
-    dbAddresses = [];
-    
-    // Read out selected territory
-    var menu = document.getElementById("termenu");
-    var tername = menu.options[menu.selectedIndex].value;
-    var res = db.exec("SELECT * FROM master WHERE tername = \"" + tername + "\";");
-    
-    writeTableHeaderRow();
-    
-    var index = 1;
-    for (var i = 0; i < res[0].values.length && i < maxTableSize; i++)
-    {
-        var rowData = res[0].values[i];
-        index = writeTableRow(rowData, index);
-    }
-
-    generateMap();
-}
-
 function writeTableHeaderRow()
 {
     var table = document.getElementById("tertable");
@@ -247,15 +184,6 @@ function writeTableRow(rowData, index)
     }
     
     return index + 1;
-}
-
-function generateMap()
-{
-    if (map == null)
-    {
-        createMap();
-    }
-    refreshMap();
 }
 
 function checkinTerritory()
